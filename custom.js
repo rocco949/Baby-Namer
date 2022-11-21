6b196b2fb735355342c2781577248ed463624257
rocco949 <118591147+rocco949@users.noreply.github.com>
2022-11-20 18:26:35 -0800

    Added essential JS

---
+++ custom.js
@@ -0,0 +1,12 @@
+const possible_first_names = []
+const possible_last_names = []
+
+function changer() {	
+	let firstN = Math.floor(Math.random() * possible_first_names.length)
+	let lastN = Math.floor(Math.randomm() * possible_last_names.length)
+	let names = [document.getElementById(first_name, last_name)]
+
+	names[0] = firstN;
+	names[1] = lastN;
+		
+	}
