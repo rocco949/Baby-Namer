e6802b96a9fd331d80b59e017aab937343fda1a8
rocco949 <118591147+rocco949@users.noreply.github.com>
2022-11-21 17:37:26 -0800

    finishing idea and making sure it fits

--- custom.js
+++ custom.js
@@ -1,12 +1,12 @@
-const possible_first_names = []
-const possible_last_names = []
+const boy_names = ["Harry", "Puggy", "Zane", "Galby", "Rex", "Tex", "Siv"]
 
-function changer() {	
-	let firstN = Math.floor(Math.random() * possible_first_names.length)
-	let lastN = Math.floor(Math.randomm() * possible_last_names.length)
-	let names = [document.getElementById(first_name, last_name)]
 
-	names[0] = firstN;
-	names[1] = lastN;
+function changer() {
+	let first_math = Math.floor(Math.random() * (boy_names.length + 1))
+	
+	let names = [document.getElementById(boy), document.getElementById(girl)]
+
+	names[0].innerHTML = boy_names[firstN];
+	names[1].innerHTML = possible_last_names[lastN]; 
 		
-	}
+}
